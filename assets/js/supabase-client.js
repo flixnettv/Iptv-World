507af04d6dc805839264db0735cbe28f05c21fc3
flixnettv <51333756+flixnettv@users.noreply.github.com>
2026-02-19 22:30:32 +0200

    Last Sync: 2026-02-19 22:30 (Mobile)

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -1,5 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://kiygsjsaxijwebqwmccb.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = sb_publishable_cD6J1pox3KmDlBoeqsSapg_Y-zkbPp3
 const supabase = createClient(supabaseUrl, supabaseKey)
