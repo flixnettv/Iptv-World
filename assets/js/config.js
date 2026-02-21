89b9cbdaa662bcccc255cfd057d3e6aed029808a
flixnettv <51333756+flixnettv@users.noreply.github.com>
2026-02-21 11:55:40 +0200

    Last Sync: 2026-02-21 11:55 (Mobile)

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,4 @@
-// Supabase Configuration
 window.SUPABASE_CONFIG = {
   url: 'https://kiygsjsaxijwebqwmccb.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpeWdzanNheGlqd2VicXdtY2NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NDYxNTUsImV4cCI6MjA4NzAyMjE1NX0.-dB4Wk_q_pSRyra6Pk98ke9dG_6v3KL7j7rVDi1p31A'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
 };
